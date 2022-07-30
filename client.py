9e0d138b815fbc67851247893b6a94f5158d08e8
PxkiDoesCoding <49614252+pxkidoescoding@users.noreply.github.com>
2022-07-30 12:04:26 -0400

    Added Encryption

--- client.py
+++ client.py
@@ -13,45 +13,50 @@ x.write(ip)
 x.close()
 #ip='127.0.0.1'
 byte=0
+def crypt(msg):
+    msg=msg[::-1]
+    tmp=b''
+    for a in msg:
+        tmp+=int(255-a).to_bytes(1,'big')
+    return tmp
 def halt():
     global wait,byte
     byte=0
     while wait:
-        tmp=server.recvfrom(4096)[0]
+        tmp=crypt(server.recvfrom(4096)[0])
         byte+=len(tmp)
         tmp=tmp.decode('utf-8')
         if tmp=='EOL':
             wait=0
         if wait:
             print(tmp)
-server.sendto(bytes('motd','utf-8'),(ip,7000))
+server.sendto(crypt(bytes('motd','utf-8')),(ip,7000))
 wait=0
-tmp=server.recvfrom(4096)[0]
+tmp=crypt(server.recvfrom(4096)[0])
 server.settimeout(30)
 psd='No Name'
 if tmp.decode('utf-8')=='SOL':
     wait=1
     halt()
-        
 while True:
     try:
-        server.sendto(bytes('whoami','utf-8'),(ip,7000))
+        server.sendto(crypt(bytes('psd','utf-8')),(ip,7000))
         wait=0
-        tmp=server.recvfrom(4096)[0]
+        tmp=crypt(server.recvfrom(4096)[0])
         if tmp.decode('utf-8')=='SOL':
             wait=1
         while wait:
-            tmp=server.recvfrom(4096)[0]
+            tmp=crypt(server.recvfrom(4096)[0])
             tmp=tmp.decode('utf-8')
             if tmp=='EOL':
                 wait=0
             if wait:
                 psd=tmp
         tmp=''
-        cmd=str(input(str(psd)+' $'))
-        server.sendto(bytes(cmd,'utf-8'),(ip,7000))
+        cmd=str(input(str(psd)))
+        server.sendto(crypt(bytes(cmd,'utf-8')),(ip,7000))
         wait=0
-        tmp=server.recvfrom(4096)[0]
+        tmp=crypt(server.recvfrom(4096)[0])
 #        print('['+str(tmp)+']')
         if tmp.decode('utf-8')=='SOL':
             wait=1
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+from genericpath import isdir
 import os,urllib.request,sys,psutil,subprocess,platform
 from socket import *
-forbid=['ping']
-#forbid=['analsyde']
+#forbid=['ping']
+forbid=['analsyde']
 #rootfs='rootfs/'
 #binpath=rootfs+'bin/'
 #libpath=rootfs+'lib/'
@@ -10,10 +11,11 @@ forbid=['ping']
 #settingspath=rootfs+'etc/'
 #homepath=rootfs+'user/'
 #path=[rootfs,binpath,libpath,sharepath,x16path,settingspath,homepath]
-version=0.002*2
+version=0.002*7
 rpnt=print
 osname='LShell'
 motd='Welcome to '+str(os.name).upper()+' '+str(platform.release())
+commands=['help - Show Help Page :>','osstat - Check OS Version','shellstat - Check Shell Version','cpustat - Check CPU Usage','ramstat - Check Ram Pressure']
 def execute(cmd,port):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
     for stdout_line in iter(popen.stdout.readline, ""):
@@ -22,15 +24,15 @@ def execute(cmd,port):
     return_code = popen.wait()
     if return_code:
         pass
-def encrypt(msg):
+def crypt(msg):
+    msg=msg[::-1]
     tmp=b''
     for a in msg:
-        print(a)
         tmp+=int(255-a).to_bytes(1,'big')
     return tmp
 def pnt(msg):
     rpnt(str(msg)+' to ('+str(address)+')')
-    server.sendto(bytes(msg,'utf-8'),address)
+    server.sendto(crypt(bytes(msg,'utf-8')),address)
 def abr(size):
     if size>=1000000000000:
         return str(size//1000000000000)+' TB'
@@ -62,10 +64,57 @@ def shell(cmd):
 #        for a in path:
 #            a=a.replace('\n','')
 #            pnt(a)
+    elif cmd[0]=='help':
+        pnt('Commands:')
+        for a in commands:
+            pnt(a)
+    elif cmd[0]=='.'*255:
+        if len(cmd)>1:
+            if cmd[1]=='..':
+                if os.name=='nt':
+                    cwdtmp=cwd.split('\\')
+                else:
+                    cwdtmp=cwd.split('/')
+                if not len(cwdtmp)<2:
+                    cwdtmp=cwdtmp[:len(cwdtmp)-1]
+                    cwd=''
+                    for a in cwdtmp:                       
+                        if os.name=='nt':
+                            cwd+=a+'\\'
+                        else:
+                            cwd+=a+'/'
+                    pnt('Moved to '+cwd)
+                else:
+                    pnt('Already at Root')
+            elif os.path.isdir(cmd[1]):
+                pnt('Moved to '+cmd[1])
+                cwd=cmd[1]
+        else:
+            pnt(cwd)
+    elif cmd[0]=='ls' or cmd[0]=='dir':
+        for a in os.listdir():
+            pnt('['+str(a)+']')
+    elif cmd[0]=='connect':
+        if address in adrs:
+            pnt('No Need to Reconnect')
+        else:
+            pnt(motd)
+            pnt('SET-'+psd)
+    elif cmd[0]=='set':
+        if not len(cmd)>1:
+            pnt('Needs 2 Arguments')
+        else:
+            tok=''
+            for a in cmd[1:]:
+                tok+=a+' '
+            pnt('SET-'+tok)
+            
     elif cmd[0]=='osstat':
         pnt(str(os.name).upper()+' '+str(platform.release()))
     elif cmd[0]=='shellstat':
-        pnt(osname+' '+str(version)+'\nBuilt on 07/28/2022')
+        pnt(osname+' '+str(version)+'\nBuilt on 07/29/2022')
+    elif cmd[0]=='psd':
+        pnt(psd)
     elif cmd[0]=='cpustat':
         pnt('CPU USAGE:'+str(psutil.cpu_percent())+'%')
     else:
@@ -98,25 +147,50 @@ def shell(cmd):
         else:
             pnt('This is Disabled Cause: in Forbid Section')
     pnt('EOL')
-def create_user(user):
-    if not os.path.isdir(homepath+user):
-        os.mkdir('')
-    else:
-        pnt(user+' Already Exists')
+def engl(cmd):
+    if cmd=='whoami':
+        return str(address[0])
+#def create_user(user):
+#    if not os.path.isdir(homepath+user):
+#        os.mkdir('')
+#    else:
+#        pnt(user+' Already Exists')
 #for a in path:
 #    if not os.path.isdir(a):
 #        os.mkdir(a)
 #        rpnt('Created '+a)
-rpnt('Starting',osname,'Remote Server... (28072022)')
+username=''
+hostname=''
+cwd='~'
+def check():
+    global username, hostname, cwd, psd
+    username=str(engl('whoami'))
+    hostname=str(execute('hostname','0'))
+    cwd='~'
+if os.path.isfile('psd'):
+    rpnt('Found psd file using that as PSD')
+    psd=open('psd').read().replace('\n','').replace('+cwd+',cwd).replace('+username+',username).replace('+hostname',hostname)
+else:
+    psd='~ $'
+rpnt('Starting',osname,'Remote Server...')
 server=socket(AF_INET,SOCK_DGRAM)
 server.bind(('0.0.0.0',7000))
 rpnt('Started on Port 7000')
+adrs=[]
 while 1:
     message, address=server.recvfrom(4096)
+    message=crypt(message)
     try:
         message.decode('utf-8')
+        if not address[0] in adrs:
+            adrs=[address[0]]
+            check()
+        psd=open('psd').read().replace('\n','').replace('+cwd+',cwd).replace('+username+',username).replace('+hostname',hostname)
         shell(message.decode('utf-8'))
     except Exception as error:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(str(exc_tb.tb_lineno)+' Error [This is hidden to client]')
         pnt(str(error))
         pnt('EOL')
 #    rpnt(address)
